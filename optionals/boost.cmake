f187788214b0934b51dad64fce3818d62f350635
Pixel <scott.blatt.0b10@gmail.com>
2024-10-18 11:56:47 -0500

    boost

---
+++ optionals/boost.cmake
@@ -0,0 +1,25 @@
+# Ensure that CPM is available
+if(NOT "cpm" IN_LIST CMAKE_LIBS_OPTIONALS)
+    list(APPEND CMAKE_LIBS_OPTIONALS "cpm")
+    include(${CMAKE_CURRENT_LIST_DIR}/cpm.cmake)
+endif()
+if(NOT CMAKE_LIBS_OPTIONALS_BOOST_COMPONENTS)
+    message(FATAL_ERROR "Boost components need to be specified")
+endif()
+if(NOT CMAKE_LIBS_OPTIONALS_BOOST_VERSION)
+    message(FATAL_ERROR "Boost version needs to be specified")
+endif()
+
+find_package(
+    Boost ${CMAKE_LIBS_OPTIONALS_BOOST_VERSION}
+    COMPONENTS ${CMAKE_LIBS_OPTIONALS_BOOST_COMPONENTS}
+)
+
+if(NOT Boost_FOUND)
+    # Use cpm if a local version can't be used
+    set(BOOST_INCLUDE_LIBRARIES CMAKE_LIBS_OPTIONALS_BOOST_COMPONENTS)
+    CPMAddPackage(
+        NAME Boost
+        URL "https://github.com/boostorg/boost/releases/download/boost-${CMAKE_LIBS_OPTIONALS_BOOST_VERSION}/boost-${CMAKE_LIBS_OPTIONALS_BOOST_VERSION}.tar.xz"
+    )
+endif()
