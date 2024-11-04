744ee63c33a8841aad5d775343c8372ac2030db5
Pixel <scott.blatt.0b10@gmail.com>
2024-11-04 12:21:46 -0600

    add c++20 modules

--- config.cmake
+++ config.cmake
@@ -2,6 +2,7 @@ set(CMAKE_CXX_STANDARD 23)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS OFF)
 set(CMAKE_EXPORT_COMPILE_COMMANDS true)
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fmodules-ts -fmodule-header") # Enable c++20 modules
 # Allow for better debug messages using the preprocessor
 # Just use the __FILENAME__ macro
 # Broken atm
