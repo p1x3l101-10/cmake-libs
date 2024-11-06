13f693fb1c95586328db7765838458f84bc8a0ef
Pixel <scott.blatt.0b10@gmail.com>
2024-11-06 11:04:55 -0600

    clangopts

--- config.cmake
+++ config.cmake
@@ -3,7 +3,7 @@ set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS OFF)
 set(CMAKE_EXPORT_COMPILE_COMMANDS true)
 if(USE_CPP_20_IMPORTS)
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fmodules-ts -fmodule-header") # Enable c++20 modules
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fmodules-ts -fmodule-header -stdlib=libc++") # Enable c++20 modules
     set(CMAKE_EXPERIMENTAL_CXX_IMPORT_STD on)
 endif()
 # Allow for better debug messages using the preprocessor
